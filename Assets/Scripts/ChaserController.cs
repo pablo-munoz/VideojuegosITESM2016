0bdc47d7837304fbb6a9d42c0e3f0e052debd659
Eduardo Merino <A01630088@itesm.mx>
2016-11-17 13:55:47 -0600

    enemyChaser
    Created an enemy that chases the player.
    Needs correction, impact walls.

---
+++ Assets/Scripts/ChaserController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaserController : MonoBehaviour {
+
+	//necesita hacer contacto con las paredes.
+
+	public float treshold = 0.7f;
+	public float speed = 0.7f;
+	public Transform player;
+	public float separation;
+	public float rangeOfView;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		separation = Vector2.Distance(transform.position, player.position);
+
+		if (separation < rangeOfView) {
+			transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+		}
+		Debug.Log (separation);
+			
+	}
+}
